03d13fa1cfe97defcd20fa1c677660ac30901e49
lordmyshepherd <lordmyshepherd29@gmail.com>
2021-11-22 18:39:58 +0900

    WIP

--- users/views.py
+++ users/views.py
@@ -1,64 +1,43 @@
-import json, re, bcrypt, jwt
+class Kakao:
+    # Class = Property(Data) + Method(Action)
+    def __init__(self, access_token):
+        self.access_token  = access_token
+        self.user_info_api = "https://kapi.kakao.com/v2/user/me"
+        self.map_api       = "https://kapi.kakao.com/v2/kakao-map/address"
+    
+    @property
+    def user_information(self):
+        response = requests.get(
+            self.user_info_api, 
+            headers = {"Authorization": f"Bearer {self.access_token}"},
+            timeout = 3
+        )
 
-from django.http      import JsonResponse
-from django.views     import View
+        if not response.status_code == 200:
+            raise Exception("ss")
 
-from config.settings  import SECRET_KEY, ALGORITHM
-from users.models     import User
+        return response.json()
+
+    def get_current_address(self, lat, lng):
+        return requests.get(self.map_api + f"?latitude={lat}&logitude={lng}")
 
 class SignUpView(View):
     def post(self, request):
         try:
-            data              = json.loads(request.body)
-            email             = data["email"]
-            password          = data["password"]
-            nickname          = data["nickname"]
-            profile_image_url = data["profile_image_url"]
-            description       = data["description"]
-            login_platform    = "None"
-            login_platform_id = "None"
+            kakao        = Kakao(request.headers["Authorization"])
+            kakao_user   = kakao.user_information
 
-            if not re.match("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email):
-                return JsonResponse({"MESSAGE" : "INVALD_EMAIL"}, status=400)
-            
-            if not re.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$", password):
-                return JsonResponse({"MESSAGE" : "INVALD_PASSWORD"}, status=400)
-        
-            if User.objects.filter(email=email).exists():
-                return JsonResponse({"MESSAGE" : "EMAIL_EXISTS"}, status=400)
-
-            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
-
-            User.objects.create(
-                email             = email,
-                password          = hashed_password,
-                nickname          = nickname,
-                profile_image_url = profile_image_url,
-                description       = description,
-                login_platform    = login_platform,
-                login_platform_id = login_platform_id,
-            )        
-            return JsonResponse({"MESSAGE" : "SUCCESS"}, status=201)
-       
-        except KeyError:
-            return JsonResponse({"MESSAGE" : "KEY_ERROR"}, status=400)
-
-class SignInView(View):
-    def post(self, request):
-        try:
-            data        = json.loads(request.body)
-            email       = data["email"]
-            password    = data["password"]
-            user        = User.objects.get(email=email)
-
-            if not bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
-                return JsonResponse({"MESSAGE":"INVALID_USER"}, status=401)
+            user, created = User.objects.get_or_create(
+                login_platform_id = kakao_user["id"],
+                defaults = {
+                    "email"             : kakao_user["kakao_account"]["email"],
+                    "login_platform"    : "kakao",
+                }
+            )
 
             access_token = jwt.encode({"id" : user.id}, SECRET_KEY, algorithm = ALGORITHM)
-            return JsonResponse({"MESSAGE":"SUCCESS", "ACCESS_TOKEN": access_token}, status=200)
 
+            return JsonResponse({"MESSAGE":"SUCCESS", "ACCESS_TOKEN": access_token}, status=200)
+            
         except KeyError:
             return JsonResponse({"MESSAGE":"KEY_ERROR"}, status=400)
-
-        except User.DoesNotExist:
-            return JsonResponse({"MESSAGE":"USER_DOES_NOT_EXIST"}, status=404)
