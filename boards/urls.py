6de9ca35d984bf56f33fc3a984564d8d165f8295
Jaewon Jang <jewon119@gmail.com>
2021-11-24 10:54:47 +0900

    Modified: Using prefetch_related in BoardDetailView

--- boards/urls.py
+++ boards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views      import BoardListView, MyBoardsView, PinListView
+from .views      import BoardListView, MyBoardsView, PinListView, BoardDetailView
 
 urlpatterns = [
     path("", BoardListView.as_view()),
     path("/pin", PinListView.as_view()),
     path("/board/me", MyBoardsView.as_view()),
+    path("/<int:board_id>", BoardDetailView.as_view()),
 ]
